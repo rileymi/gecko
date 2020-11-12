7e55ad75e4b8c23ae3acc8665052a6741be64b03
rileymi <michael@michael-riley.com>
2020-11-12 07:24:12 +0000

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo "Installing Gecko - The Development Environment Bootstrapper for the Mac"
+
+# Install Homebrew
+# See: https://brew.sh
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
